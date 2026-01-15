f6a31523183f3eeb03499cd5ad78d7e4c62d77ae
methodiusdev <methodius.dev@gmail.com>
2026-01-15 14:19:51 +0100

    Introduce simple streams

---
+++ streams.lisp
@@ -0,0 +1,13 @@
+(defmacro delay (expr)
+  `(lambda () ,expr))
+
+(defun force (delayed-expr)
+  (funcall delayed-expr))
+
+(defmacro cons-stream (head tail)
+  `(cons ,head (delay ,tail)))
+
+(defun stream-car (stream) (car stream))
+(defun stream-cdr (stream) (force (cdr stream)))
+
+
