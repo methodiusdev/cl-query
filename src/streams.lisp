c406b2c55f3636f26b0d1174b302448f49daf3ed
methodiusdev <methodius.dev@gmail.com>
2026-01-15 23:04:03 +0100

    Docstrings

--- src/streams.lisp
+++ src/streams.lisp
@@ -1,12 +1,17 @@
 (in-package :cl-query)
 
 (defmacro delay (expr)
+  "Delays evaluation of EXPR by wrapping it in a lambda.
+   Returns a promise (function with no arguments)."
   `(lambda () ,expr))
 
 (defun force (delayed-expr)
+  "Forces evaluation of a delayed expression.
+   DELAYED-EXPR should be a promise created by DELAY."
   (funcall delayed-expr))
 
 (defmacro cons-stream (head tail)
+  "Constructs a stream with HEAD (evaluated) and TAIL (delayed)."
   `(cons ,head (delay ,tail)))
 
 (defun stream-car (stream) (car stream))
